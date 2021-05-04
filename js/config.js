4a8b41eef34388e885b935b9bd0bcd0646eee218
kashgod <aakashgoud97@gmail.com>
2021-05-05 02:16:58 +0530

    New connection from aakash-alias account

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8HgQ4ofrl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '58o8a4hnjiqpe3qchsgn0irnnp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_SMcTPD0DG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '51ofmf67sott90bdt1d7ltoh4j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
